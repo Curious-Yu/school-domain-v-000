bbe39730f8df5908ac357d2960eb81ea2c9cb2af
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 19:13:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,14 @@
 # code here!
 class School
 
+  attr_accessor :roster
+
   def initialize(name,roster)
     @name = name
     @roster = {}
   end
 
-  attr_accessor :roster
+
 
   def add_student(name, grade)
     @roster[grade] = [] if @roster[grade].nil?
